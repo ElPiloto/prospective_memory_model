e1ddc539c6af3d6c1b3c136b05265633c8ab20d5
Luis R. Piloto <luis.r.piloto@gmail.com>
2014-01-26 20:13:24 -0500

    Increase EM noise to see if WM PI effect increases

--- runSimYesDecayYesRehearseYesEMnoise.m
+++ runSimYesDecayYesRehearseYesEMnoise.m
@@ -1,5 +1,5 @@
 simulation = Trial_Simulator();
-simulation.EMencodingNoise = 0.8;
+simulation.EMencodingNoise = 0.7;
 
 % this makes it so that we're always comparing the WM trace (as it was initially encoded - no decay, no retrieval of an EM trace)
 simulation = simulation.ILL_SIM_YOU_LATER();
